6c8202f26ec1bdb32ded512f9cc9dce710617af4
Mark Kingsbury <mark@kingsburydata.com>
2022-12-29 17:25:04 +0000

    first bits to trigger TF PLAN

---
+++ environments/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0f540e9f488cfa27d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+terraform {
+  backend "s3" {
+  }
+}
