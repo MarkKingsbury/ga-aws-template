24f8b7f197747158e0e9db50c8f46d6157cd5bd4
Mark Kingsbury <mark@kingsburydata.com>
2023-01-03 13:08:48 +0000

    plan.txt instead

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -21,4 +21,4 @@ ls -la
 terraform plan -input=false -out plan.out > /dev/null 2> error.txt
 #terraform plan -var-file="terraform.tfvars.json" -input=false -out plan.out > /dev/null 2> error.txt
 terraform show -no-color plan.out > plan.txt
-aws s3 cp plan.out "${TERRAFORM_PLAN_BUCKET}"/"${ENV}"/"${TRIGGERING_ACTOR}"/ > /dev/null 2> error.txt
+aws s3 cp plan.txt "${TERRAFORM_PLAN_BUCKET}"/"${ENV}"/"${TRIGGERING_ACTOR}"/ > /dev/null 2> error.txt
