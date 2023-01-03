128e3f60d31d7c5629c44dcf6232eaa2813ddb86
Mark Kingsbury <mark@kingsburydata.com>
2023-01-03 10:25:24 +0000

    put init back

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,6 +5,6 @@ aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_BACKEND_KEY}" --with-decrypt
 #aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_VARS_KEY}" --with-decryption --query "Parameters[*].Value" --output text > terraform.tfvars.json 2> error.txt
 echo "after get-parameters"
 cat backend.conf > error.txt
-#terraform init -backend-config=backend.conf -reconfigure > /dev/null 2> error.txt
+terraform init -backend-config=backend.conf -reconfigure > /dev/null 2> error.txt
 #terraform workspace list > /dev/null 2> error.txt
 #terraform workspace select "${ENV}" > /dev/null 2> error.txt
