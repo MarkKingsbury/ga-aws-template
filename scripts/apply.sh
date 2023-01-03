844be0ea5b2070482d9fe53a319f755529bb5d3c
Mark Kingsbury <mark@kingsburydata.com>
2023-01-03 13:34:21 +0000

    add apply script

---
+++ scripts/apply.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+
+echo
+function cleanup {
+    status=$?;
+    if [ $status != 0 ] && [ -f error.txt ]; then echo "Failure: $status" && python3 ../scripts/send_to_cw.py error.txt; fi
+}
+
+trap cleanup EXIT
+
+ls -la
+pwd
+cd "${TF_DIR}"
+pwd
+ls -la
+sh ../scripts/init.sh
+#terraform plan -var-file="terraform.tfvars.json" -input=false -out plan.out > /dev/null 2> error.txt
+aws s3 cp "${TERRAFORM_PLAN_BUCKET}"/"${ENV}"/"${TRIGGERING_ACTOR}"/plan.out plan.out > /dev/null 2> error.txt
+terraform apply -input=false plan.out > apply.out 2> error.txt
