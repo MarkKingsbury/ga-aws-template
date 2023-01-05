55106802f3f9809ccbfa8138370fd6098c02b241
Mark Kingsbury <mark@kingsburydata.com>
2023-01-05 14:23:22 +0000

    log plan to cw

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -20,4 +20,6 @@ echo "get parameters"
 aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_VARS_KEY}" --with-decryption --query "Parameters[*].Value" --output text > terraform.tfvars.json 2> error.txt
 ls -la
 terraform plan -var-file="terraform.tfvars.json" -input=false -out plan.out > /dev/null 2> error.txt
+terraform show -no-color plan.out > plan.txt
+python3 ../scripts/send_to_cw.py plan.txt
 aws s3 cp plan.out "${TERRAFORM_PLAN_BUCKET}"/"${ENV}"/"${TRIGGERING_ACTOR}"/ > /dev/null 2> error.txt
